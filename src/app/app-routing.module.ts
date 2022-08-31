e28e352ae2d544e1ef15abb1c975d046a8285297
ajmalrahmannc <ajmalrahman430@gmail.com>
2022-08-31 18:27:33 +0530

    login and register added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+<<<<<<< HEAD
 import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
@@ -14,6 +15,14 @@ const routes: Routes = [
   {
     path:"login",component:LoginComponent
   }
+=======
+import { FooterComponent } from './Components/footer/footer.component';
+import { HeaderComponent } from './Components/header/header.component';
+
+const routes: Routes = [
+  {path:'header',component:HeaderComponent},
+  {path:'footer',component:FooterComponent}
+>>>>>>> 124ecb8efb9da87e0a0e59e5b70cdc55b3450480
 ];
 
 @NgModule({
