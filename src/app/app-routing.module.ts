c939d14c22149c1c644f039c676edadc63db1064
Russo7 <aneetasolaman1999@gmail.com>
2022-09-05 12:12:57 +0530

    modified about us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { AboutUsComponent } from './Components/about-us/about-us.component';
-=======
 import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
->>>>>>> d4076a7910a2508423de96a378c997771e7a0b69
 import { FooterComponent } from './Components/footer/footer.component';
 import { HeaderComponent } from './Components/header/header.component';
 
 const routes: Routes = [
-<<<<<<< HEAD
   {path:'header',component:HeaderComponent},
   {path:'footer',component:FooterComponent},
-  {path:'about-us',component:AboutUsComponent}
-];
-=======
+  {path:'about-us',component:AboutUsComponent},
   {
     path:"",component:HomeComponent
   },
@@ -26,10 +20,9 @@ const routes: Routes = [
   {
     path:"login",component:LoginComponent
   },
-   {path:'header',component:HeaderComponent},
-  {path:'footer',component:FooterComponent}
+];
 
->>>>>>> d4076a7910a2508423de96a378c997771e7a0b69
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { HomeComponent } from './Components/home/home.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { LoginComponent } from './Components/login/login.component';
-<<<<<<< HEAD
 import { AboutUsComponent } from './Components/about-us/about-us.component';
-=======
 import { RegisterComponent } from './Components/register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
->>>>>>> d4076a7910a2508423de96a378c997771e7a0b69
 
 @NgModule({
   declarations: [
@@ -21,11 +17,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     HomeComponent,
     FooterComponent,
     LoginComponent,
-<<<<<<< HEAD
-    AboutUsComponent
-=======
-    RegisterComponent
->>>>>>> d4076a7910a2508423de96a378c997771e7a0b69
+    AboutUsComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
